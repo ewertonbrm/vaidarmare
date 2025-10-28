761e741b24f102e6ea7b2893cb48904ac3a7c6ee
Ewerton Melo <88634234+ewertonbrm@users.noreply.github.com>
2025-10-28 12:57:39 -0300

    Update vercel.js

--- vercel.js
+++ vercel.js
@@ -5,4 +5,33 @@
       "destination": "https://api.qweather.com/v7/ocean/tide"
     }
   ]
+    "headers": [
+      {
+        "source": "/service-worker.js",
+        "headers": [
+          {
+            "key": "Cache-Control",
+            "value": "no-cache"
+          },
+          {
+            "key": "Service-Worker-Allowed",
+            "value": "/"
+          }
+        ]
+      },
+      {
+        "source": "/manifest.json",
+        "headers": [
+          {
+            "key": "Content-Type",
+            "value": "application/manifest+json"
+          },
+          {
+            "key": "Cache-Control",
+            "value": "public, max-age=0, must-revalidate"
+          }
+        ]
+      }
+    ]
 }
+
