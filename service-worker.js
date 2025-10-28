f93c8c99350a893f04066a2aacee517dadb3ca6c
Ewerton Melo <88634234+ewertonbrm@users.noreply.github.com>
2025-10-27 23:43:27 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'torrentum-pwa-cache-v1';
+// CAMINHOS AJUSTADOS
+const urlsToCache = [
+    '/vaidarmare/',
+    '/vaidarmare/index.html',
+    '/vaidarmare/manifest.json',
+    '/vaidarmare/icons/icon-192x192.png',
+    '/vaidarmare/icons/icon-512x512.png'
+    // Adicione o caminho do seu GIF aqui se estiver local
+    // '/insta/caminho/do/seu/gif.gif' 
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Service Worker: Arquivos em cache durante a instalação');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  // Ignora requisições para o site externo (https://androidauthority.com)
+  if (!event.request.url.includes(self.location.origin)) {
+      return;
+  }
+  
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+
+});
